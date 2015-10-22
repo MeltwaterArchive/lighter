ceedf0fc488d73be9a9ae6d1614fb0992d648910
Rasmus Johansson <rasmus.johansson@meltwater.com>
2015-10-22 15:38:48 +0200

    Added authentication support

--- lighter.py
+++ lighter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, sys, optparse, logging
 from pprint import pprint
-import yaml, urllib2, json
+import yaml, urllib2, json, urlparse, base64
 
 def parsebool(value):
     truevals = set(['true', '1'])
@@ -25,32 +25,64 @@ def parseint(value):
 def parselist(value):
     return filter(bool, value.split(','))
 
-def merge(a, b):
+def merge_dicts(a, b):
     result = {}
 
     for key in set(a.keys() + b.keys()):
         aval = a.get(key)
         bval = b.get(key)
         if isinstance(aval, dict) or isinstance(bval, dict):
-            result[key] = merge(aval or {}, bval or {})
+            result[key] = merge_dicts(aval or {}, bval or {})
         else:
             result[key] = bval or aval
 
     return result
+
+def urlunparse(data):
+    """
+    Modified from urlparse.urlunparse to support file://./path/to urls
+    """
+    scheme, netloc, url, params, query, fragment = data
+    if params:
+        url = "%s;%s" % (url, params)
+    if netloc:
+        url = '//' + (netloc or '') + url
+    if scheme:
+        url = scheme + ':' + url
+    if query:
+        url = url + '?' + query
+    if fragment:
+        url = url + '#' + fragment
+    return url
+
+def build_request(url, data=None, headers={}, method='GET'):
+    parsed_url = urlparse.urlparse(url)
+    parts = list(parsed_url[0:6])
+    parts[1] = ('@' in parts[1]) and parts[1].split('@')[1] or parts[1]
+
+    request = urllib2.Request(urlunparse(parts), data, headers)
+    request.get_method = lambda: method
+
+    if parsed_url.username is not None and parsed_url.password is not None:
+        # You need the replace to handle encodestring adding a trailing newline
+        # (https://docs.python.org/2/library/base64.html#base64.encodestring)
+        base64string = base64.encodestring('%s:%s' % (parsed_url.username, parsed_url.password)).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
+
+    return request
 
 def parse_file(file):
     with open(file, 'r') as stream:
         doc = yaml.load(stream)
         repository = doc['maven']['repository']
         url = '{0}/{1}/{2}/{3}/{2}-{3}.json'.format(repository, doc['maven']['groupid'].replace('.', '/'), doc['maven']['artifactid'], doc['maven']['version'])
-        response = urllib2.urlopen(url).read()
+        response = urllib2.urlopen(build_request(url)).read()
         json_response = json.loads(response)
-        merged_content = merge(json_response, doc['override'])
+        merged_content = merge_dicts(json_response, doc['override'])
         return merged_content
 
-
 def get_marathon_url(url, id):
-    return url.rstrip('/') + '/v2/apps/' + id.strip('/')
+    return url.rstrip('/') + '/v2/apps/' + id.strip('/') + '?force=true'
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(
@@ -77,9 +109,7 @@ if __name__ == '__main__':
     for file in args:
         file_content = parse_file(file)
         serialized_json = json.dumps(file_content)
-        pprint(serialized_json)
+
         request = urllib2.Request(get_marathon_url(options.marathon, file_content['id']), serialized_json, {'Content-Type': 'application/json'})
         request.get_method = lambda: 'PUT'
         response = urllib2.urlopen(request)
-        pprint(response.read())
-
--- test.py
+++ test.py
@@ -3,23 +3,28 @@ import unittest, lighter
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
+    def test_parse_file(self):
         json_file = lighter.parse_file('test/myservice.yml')
         self.assertEqual(json_file['id'],'myservice')
         self.assertEqual(json_file['env']['DATABASE'], 'database:3306')
         self.assertEqual(json_file['cpus'], 1)
 
-    def test_merge_two_dicts(self):
+    def test_merge_dicts(self):
         x = {'a': 1, 'b': 2}
         y = {'b': 3, 'c': 4}
         x_y = {'a': 1, 'b': 3, 'c': 4}
-        self.assertEqual(lighter.merge(x, y),x_y)
+        self.assertEqual(lighter.merge_dicts(x, y),x_y)
         y_x = {'a': 1, 'b': 2, 'c': 4}
-        self.assertEqual(lighter.merge(y, x),y_x)
+        self.assertEqual(lighter.merge_dicts(y, x),y_x)
 
     def test_get_marathon_url(self):
-        self.assertEqual(lighter.get_marathon_url('myurl', 'myid'), 'myurl/v2/apps/myid')
-        self.assertEqual(lighter.get_marathon_url('myurl/', '/myid/'), 'myurl/v2/apps/myid')
+        self.assertEqual(lighter.get_marathon_url('myurl', 'myid'), 'myurl/v2/apps/myid?force=true')
+        self.assertEqual(lighter.get_marathon_url('myurl/', '/myid/'), 'myurl/v2/apps/myid?force=true')
+
+    def test_build_request(self):
+        url = "https://user:pass@maven.example.com/path/to/my/repo"
+        req = lighter.build_request(url)
+        self.assertTrue(req.get_header('Authorization').startswith('Basic '))
 
 if __name__ == '__main__':
     unittest.main()
