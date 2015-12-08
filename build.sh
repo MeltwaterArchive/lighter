67431277c8176eb62779de6f36fec99c9b76a823
Alexander Nilsson <alxnlssn@gmail.com>
2015-12-08 15:15:28 +0100

    add switch to the build script to only run the tests

--- build.sh
+++ build.sh
@@ -31,5 +31,8 @@ fi
 # Run unit tests
 ./test
 
-# Build the standalone binary
-pyinstaller --onefile "--name=lighter-$(uname -s)-$(uname -m)" "--additional-hooks-dir=`dirname $0`/src/hooks" src/lighter/main.py
+# if "test" is provided as an argument, skip building the binary
+if [ "$1" != "test" ]; then
+    # Build the standalone binary
+    pyinstaller --onefile "--name=lighter-$(uname -s)-$(uname -m)" "--additional-hooks-dir=`dirname $0`/src/hooks" src/lighter/main.py
+fi
