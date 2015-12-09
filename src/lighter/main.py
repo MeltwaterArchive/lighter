abbf9b54d1d748d7d4a399d60f72a7c94ac927d2
Alexander Nilsson <alxnlssn@gmail.com>
2015-12-09 14:30:09 +0100

    Add a password/key check over the the env variables

--- src/lighter/main.py
+++ src/lighter/main.py
@@ -78,7 +78,7 @@ class Service(object):
         return util.rget(self.document, 'variables', 'lighter.uniqueVersion') or \
                (self.image.split(':')[1] if ':' in self.image else 'latest')
 
-def parse_service(filename, targetdir=None):
+def parse_service(filename, targetdir=None, failOnPassword=False):
     logging.info("Processing %s", filename)
     with open(filename, 'r') as fd:
         document = yaml.load(fd)
@@ -120,6 +120,17 @@ def parse_service(filename, targetdir=None):
     except KeyError, e:
         raise RuntimeError('Failed to parse %s with the following message: %s' % (filename, str(e.message)))
 
+    # Check for unencrypted passwords and keys
+    if 'env' in config:
+        for item in config['env'].iteritems():
+            key = item[0].lower()
+            if (('password' in key or 'pwd' in key or 'key' in key) and not 'public' in key) and not item[0].startswith('ENC['):
+                if failOnPassword:
+                    raise RuntimeError('Found unencrypted password/key in %s: %s' % (filename, item[0]))
+                else:
+                    logging.warn('Found unencrypted password/key in %s: %s' % (filename, item[0]))
+                
+
     # Write json file to disk for logging purposes
     if targetdir:
         outputfile = os.path.join(targetdir, filename + '.json')
@@ -135,9 +146,9 @@ def parse_service(filename, targetdir=None):
 
     return Service(filename, document, config)
 
-def parse_services(filenames, targetdir=None):
+def parse_services(filenames, targetdir=None, failOnPassword=False):
     #return [parse_service(filename, targetdir) for filename in filenames]
-    return Parallel(n_jobs=8, backend="threading")(delayed(parse_service)(filename, targetdir) for filename in filenames)
+    return Parallel(n_jobs=8, backend="threading")(delayed(parse_service)(filename, targetdir, failOnPassword) for filename in filenames)
 
 def get_marathon_url(url, id, force=False):
     return url.rstrip('/') + '/v2/apps/' + id.strip('/') + (force and '?force=true' or '')
@@ -203,8 +214,8 @@ def deploy(marathonurl, filenames, noop=False, force=False, targetdir=None):
         except urllib2.URLError, e:
             raise RuntimeError("Failed to deploy %s (%s)" % (service.filename, e)), None, sys.exc_info()[2]
 
-def verify(filenames, targetdir=None):
-    parse_services(filenames, targetdir)
+def verify(filenames, targetdir=None, failOnPassword=False):
+    parse_services(filenames, targetdir, failOnPassword)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -243,6 +254,9 @@ if __name__ == '__main__':
     
     deploy_parser.add_argument('filenames', metavar='YMLFILE', nargs='+',
                        help='Service files to expand and deploy')
+    
+    deploy_parser.add_argument('-p', '--password-check-fail', dest='failOnPassword', help='Fail on password/key check triggering [default: %(default)s]',
+                      action='store_true', default=False)
 
     args = parser.parse_args()
 
@@ -255,7 +269,7 @@ if __name__ == '__main__':
         if args.command == 'deploy':
             deploy(args.marathon, noop=args.noop, force=args.force, filenames=args.filenames, targetdir=args.targetdir)
         elif args.command == 'verify':
-            verify(args.filenames, targetdir=args.targetdir)
+            verify(args.filenames, targetdir=args.targetdir, failOnPassword=args.failOnPassword)
     except RuntimeError, e:
         logging.error(str(e))
         sys.exit(1)
