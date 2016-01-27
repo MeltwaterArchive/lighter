4cdc93cb5b801b05a31a855b1ce20c4897cd0c0e
Alexander Nilsson <alxnlssn@gmail.com>
2016-01-27 13:36:18 +0100

    add cffi as hidden import

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e -x
 . ./common.sh
 
 # Build the standalone binary
-pyinstaller --hidden-import=packaging --hidden-import=packaging.version --hidden-import=packaging.specifiers --onefile "--name=lighter-$(uname -s)-$(uname -m)" "--additional-hooks-dir=`dirname $0`/src/hooks" src/lighter/main.py
+pyinstaller --hidden-import=cffi --hidden-import=packaging --hidden-import=packaging.version --hidden-import=packaging.specifiers --onefile "--name=lighter-$(uname -s)-$(uname -m)" "--additional-hooks-dir=`dirname $0`/src/hooks" src/lighter/main.py
