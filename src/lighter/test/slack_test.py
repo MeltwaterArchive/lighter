627a7856e69320da34d2a1d7829b477d301ba0a0
Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>
2018-07-02 10:59:22 +0200

    Add more test cases
    Signed-off-by: Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>

--- src/lighter/test/slack_test.py
+++ src/lighter/test/slack_test.py
@@ -1,11 +1,12 @@
 import unittest
+import urllib2
 from mock import patch, ANY
 from lighter.slack import Slack
 
 class SlackTest(unittest.TestCase):
     @patch('lighter.util.jsonRequest')
     def testNotify(self, mock_jsonRequest):
-        Slack(token='abc', channels=['123', '123', '456']).notify("Test message")
+        Slack(token='abc', channels=['123', '123', '456']).notify({'text': "Test message"})
         mock_jsonRequest.assert_any_call('https://slack.com/api/chat.postMessage', data=ANY, method='POST', headers={
             'Content-type': 'application/json; charset=UTF-8', 'Authorization': 'Bearer abc'
         })
@@ -13,3 +14,14 @@ class SlackTest(unittest.TestCase):
             'Content-type': 'application/json; charset=UTF-8', 'Authorization': 'Bearer abc'
         })
         self.assertEquals(mock_jsonRequest.call_count, 2)
+
+    @patch('lighter.util.jsonRequest')
+    def testNotifyNoToken(self, mock_jsonRequest):
+        Slack(token=None, channels=['123', '123', '456']).notify({'text': "Test message"})
+        self.assertEquals(mock_jsonRequest.call_count, 0)
+
+    @patch('lighter.util.jsonRequest')
+    def testNotifyURLError(self, mock_jsonRequest):
+        mock_jsonRequest.side_effect = urllib2.URLError("hello")
+        Slack(token=None, channels=['123', '123', '456']).notify({'text': "Test message"})
+        self.assertEquals(mock_jsonRequest.call_count, 0)
